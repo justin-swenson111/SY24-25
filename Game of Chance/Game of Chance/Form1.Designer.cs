a596a178ae8e4514802934e93fd1573dde3b74a3
Justin-Swenson <justinswenson111@gmail.com>
2024-09-13 09:02:50 -0700

    Game of chance

---
+++ Game of Chance/Game of Chance/Form1.Designer.cs
@@ -0,0 +1,260 @@
+﻿namespace Game_of_Chance
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.cherryPic = new System.Windows.Forms.PictureBox();
+            this.sevenPic = new System.Windows.Forms.PictureBox();
+            this.watermelonPic = new System.Windows.Forms.PictureBox();
+            this.lemonPic = new System.Windows.Forms.PictureBox();
+            this.barPic = new System.Windows.Forms.PictureBox();
+            this.slot1 = new System.Windows.Forms.PictureBox();
+            this.slot2 = new System.Windows.Forms.PictureBox();
+            this.slot3 = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.cashLabel = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.winningsLabel = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.cherryPic)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.sevenPic)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.watermelonPic)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.lemonPic)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.barPic)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.slot1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.slot2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.slot3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // cherryPic
+            // 
+            this.cherryPic.BackColor = System.Drawing.Color.Transparent;
+            this.cherryPic.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.cherryPic.Image = ((System.Drawing.Image)(resources.GetObject("cherryPic.Image")));
+            this.cherryPic.Location = new System.Drawing.Point(768, 12);
+            this.cherryPic.Name = "cherryPic";
+            this.cherryPic.Size = new System.Drawing.Size(20, 18);
+            this.cherryPic.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.cherryPic.TabIndex = 0;
+            this.cherryPic.TabStop = false;
+            this.cherryPic.Visible = false;
+            // 
+            // sevenPic
+            // 
+            this.sevenPic.BackColor = System.Drawing.Color.Transparent;
+            this.sevenPic.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.sevenPic.Image = ((System.Drawing.Image)(resources.GetObject("sevenPic.Image")));
+            this.sevenPic.Location = new System.Drawing.Point(768, 36);
+            this.sevenPic.Name = "sevenPic";
+            this.sevenPic.Size = new System.Drawing.Size(20, 18);
+            this.sevenPic.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.sevenPic.TabIndex = 1;
+            this.sevenPic.TabStop = false;
+            this.sevenPic.Visible = false;
+            // 
+            // watermelonPic
+            // 
+            this.watermelonPic.BackColor = System.Drawing.Color.Transparent;
+            this.watermelonPic.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.watermelonPic.Image = ((System.Drawing.Image)(resources.GetObject("watermelonPic.Image")));
+            this.watermelonPic.Location = new System.Drawing.Point(768, 60);
+            this.watermelonPic.Name = "watermelonPic";
+            this.watermelonPic.Size = new System.Drawing.Size(20, 18);
+            this.watermelonPic.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.watermelonPic.TabIndex = 2;
+            this.watermelonPic.TabStop = false;
+            this.watermelonPic.Visible = false;
+            // 
+            // lemonPic
+            // 
+            this.lemonPic.BackColor = System.Drawing.Color.Transparent;
+            this.lemonPic.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.lemonPic.Image = ((System.Drawing.Image)(resources.GetObject("lemonPic.Image")));
+            this.lemonPic.Location = new System.Drawing.Point(768, 84);
+            this.lemonPic.Name = "lemonPic";
+            this.lemonPic.Size = new System.Drawing.Size(20, 18);
+            this.lemonPic.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.lemonPic.TabIndex = 3;
+            this.lemonPic.TabStop = false;
+            this.lemonPic.Visible = false;
+            // 
+            // barPic
+            // 
+            this.barPic.BackColor = System.Drawing.Color.Transparent;
+            this.barPic.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.barPic.Image = ((System.Drawing.Image)(resources.GetObject("barPic.Image")));
+            this.barPic.Location = new System.Drawing.Point(768, 108);
+            this.barPic.Name = "barPic";
+            this.barPic.Size = new System.Drawing.Size(20, 18);
+            this.barPic.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.barPic.TabIndex = 4;
+            this.barPic.TabStop = false;
+            this.barPic.Visible = false;
+            // 
+            // slot1
+            // 
+            this.slot1.Location = new System.Drawing.Point(241, 26);
+            this.slot1.Name = "slot1";
+            this.slot1.Size = new System.Drawing.Size(100, 100);
+            this.slot1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.slot1.TabIndex = 5;
+            this.slot1.TabStop = false;
+            // 
+            // slot2
+            // 
+            this.slot2.Location = new System.Drawing.Point(377, 26);
+            this.slot2.Name = "slot2";
+            this.slot2.Size = new System.Drawing.Size(100, 100);
+            this.slot2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.slot2.TabIndex = 6;
+            this.slot2.TabStop = false;
+            // 
+            // slot3
+            // 
+            this.slot3.Location = new System.Drawing.Point(526, 26);
+            this.slot3.Name = "slot3";
+            this.slot3.Size = new System.Drawing.Size(100, 100);
+            this.slot3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.slot3.TabIndex = 7;
+            this.slot3.TabStop = false;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(8, 45);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 8;
+            this.button1.Text = "Roll";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // cashLabel
+            // 
+            this.cashLabel.AutoSize = true;
+            this.cashLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 25F);
+            this.cashLabel.Location = new System.Drawing.Point(632, 106);
+            this.cashLabel.Name = "cashLabel";
+            this.cashLabel.Size = new System.Drawing.Size(117, 48);
+            this.cashLabel.TabIndex = 9;
+            this.cashLabel.Text = "Cash";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.Black;
+            this.pictureBox1.Location = new System.Drawing.Point(13, 157);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(775, 14);
+            this.pictureBox1.TabIndex = 10;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F);
+            this.label1.Location = new System.Drawing.Point(13, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(70, 29);
+            this.label1.TabIndex = 11;
+            this.label1.Text = "Slots";
+            // 
+            // winningsLabel
+            // 
+            this.winningsLabel.AutoSize = true;
+            this.winningsLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 17.54F);
+            this.winningsLabel.Location = new System.Drawing.Point(12, 84);
+            this.winningsLabel.Name = "winningsLabel";
+            this.winningsLabel.Size = new System.Drawing.Size(0, 36);
+            this.winningsLabel.TabIndex = 12;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F);
+            this.label2.Location = new System.Drawing.Point(12, 174);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(107, 29);
+            this.label2.TabIndex = 13;
+            this.label2.Text = "Roulette";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.winningsLabel);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.cashLabel);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.slot3);
+            this.Controls.Add(this.slot2);
+            this.Controls.Add(this.slot1);
+            this.Controls.Add(this.barPic);
+            this.Controls.Add(this.lemonPic);
+            this.Controls.Add(this.watermelonPic);
+            this.Controls.Add(this.sevenPic);
+            this.Controls.Add(this.cherryPic);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.cherryPic)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.sevenPic)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.watermelonPic)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.lemonPic)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.barPic)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.slot1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.slot2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.slot3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox cherryPic;
+        private System.Windows.Forms.PictureBox sevenPic;
+        private System.Windows.Forms.PictureBox watermelonPic;
+        private System.Windows.Forms.PictureBox lemonPic;
+        private System.Windows.Forms.PictureBox barPic;
+        private System.Windows.Forms.PictureBox slot1;
+        private System.Windows.Forms.PictureBox slot2;
+        private System.Windows.Forms.PictureBox slot3;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label cashLabel;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label winningsLabel;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
